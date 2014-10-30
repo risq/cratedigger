a2e2f1bc74819a99eb6cc39c0ff8911ca67652d2
unknown <vledrapier@LIC-10.GOBELINS-ANNECY.local>
2014-10-30 10:56:46 +0100

    fix postporcessing error on resize & retina screen

--- dist/scripts/cratedigger.js
+++ dist/scripts/cratedigger.js
@@ -117,6 +117,7 @@
 
         canvasWidth,
         canvasHeight,
+        dpr,
         scrollRecordsTimeout,
         isLoading = false,
         infosPanelState = "closed",
@@ -161,7 +162,7 @@
             infoPanelOpacity: 0.9,
             postprocessing: true,
             blurAmount: 0.4,
-            updateCanvasSizeOnWindowResize: false, //does not work with postprocessing enabled
+            updateCanvasSizeOnWindowResize: true,
             infoPanelOpened: function() {},
             infoPanelClosed: function() {},
             elements: {
@@ -708,6 +709,7 @@
         dof.uniforms.tDepth.value = depthTarget;
         dof.uniforms.size.value.set(canvasWidth, canvasHeight);
         dof.uniforms.textel.value.set(1.0 / canvasWidth, 1.0 / canvasHeight);
+        FXAA.uniforms.resolution.value.set(1 / (canvasWidth * dpr), 1 / (canvasHeight * dpr));
     };
 
 
@@ -863,7 +865,7 @@
 
         FXAA = new THREE.ShaderPass(THREE.FXAAShader);
 
-        FXAA.uniforms.resolution.value.set(1 / canvasWidth, 1 / canvasHeight);
+        FXAA.uniforms.resolution.value.set(1 / (canvasWidth * dpr), 1 / (canvasHeight * dpr));
         FXAA.renderToScreen = true;
 
         composer.addPass(dof);
@@ -1146,6 +1148,13 @@
         if (!supports || !Modernizr.webgl) return;
         console.log('initializing...');
         console.log('options:', options);
+
+        if (window.devicePixelRatio !== undefined) {
+            dpr = window.devicePixelRatio;
+        }
+        else {
+            dpr = 1;
+        }
 
         rootContainerElement = document.getElementById(options.elements.rootContainerId);
         if (!rootContainerElement) {
